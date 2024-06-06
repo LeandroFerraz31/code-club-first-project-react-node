8e98c82391c77b6448f72aee5d28ca16f5d9c62f
LeandroFerraz31 <leandro.santos@universo.univates.br>
2024-06-05 21:46:18 -0300

    ajuste

---
+++ first-project-react/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import GlobalStyle from "./globalStyle";
+
+ReactDOM.render(
+  <>
+    <App /> <GlobalStyle />
+  </>,
+  document.getElementById("root")
+);
